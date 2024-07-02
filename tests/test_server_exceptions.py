6b95c52c5bb31238b518047bdf6c0ecc5012979a
Darshan Baral <DBaral@valleywater.org>
2024-07-02 10:20:44 -0700

    use real error codes

--- tests/test_server_exceptions.py
+++ tests/test_server_exceptions.py
@@ -4,7 +4,8 @@ from src.exceptions.server_exceptions import ServerError
 
 class TestServerExceptions(unittest.TestCase):
     def test_server_error(self):
-        se = ServerError("?123,456,Big Problem, Yeah!")
-        assert se.derr_no == 123
-        assert se.err_no == 456
-        assert se.message == "Big Problem, Yeah!"
+        se = ServerError("?55,0,Server requires SHA-256")
+        assert 0 <= se.derr_no <= 55
+        assert se.derr_no == 55
+        assert se.err_no == 0
+        assert se.message == "Server requires SHA-256"

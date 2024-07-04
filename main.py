c243ebb0c5a5c64f82e053d31bdbbad3650a1256
Darshan Baral <DBaral@valleywater.org>
2024-07-03 17:32:20 -0700

    move authenticate and send search criteria inside BasicClient class

--- main.py
+++ main.py
@@ -1,14 +1,10 @@
 import json
 import logging
-from datetime import datetime, timezone
-from src.logs import write_log, write_debug, write_error
+from src.logs import write_log, write_error
 from src.ldds_message import LddsMessage
 from src.basic_client import BasicClient
-from src.security import Authenticator
-from src.security import PasswordFileEntry, Sha1Hash, Sha256Hash, Hash
 from src.utils.byte_util import get_c_string
 from src.search.search_criteria import SearchCriteria
-from src.exceptions.server_exceptions import ServerError
 
 
 def test_basic_client(username: str,
@@ -24,31 +20,43 @@ def test_basic_client(username: str,
     else:
         logging.basicConfig(level=logging.INFO)
 
-    # TODO: how to set timeout
     port = port if port is not None else 16003
     timeout = timeout if timeout is not None else 30
     client = BasicClient(host=host, port=port, timeout=timeout)
-    try:
-        # requesting Authentication
-        client.connect()
-        write_log("Connected to server.")
-        authenticate_user(client, username, password)
 
+    try:
+        client.connect()
+    except Exception as e:
+        write_error("Failed to connect to server. Error: " + str(e))
+        return
+
+    try:
+        client.authenticate_user(username, password)
+    except Exception as e:
+        write_error("Failed to authenticate user. Error: " + str(e))
+        client.disconnect()
+        return
+
+    try:
         # TODO: look for error scenarios
-        # send search criteria
         criteria = SearchCriteria()
         criteria.parse_file(search_criteria)
-        send_search_crit(client=client, filename='OBJECT', data=criteria.to_string_proto(14).encode('utf-8'))
+        client.send_search_criteria(data=criteria.to_string_proto(14).encode('utf-8'))
+    except Exception as e:
+        write_error("Failed to send search criteria. Error: " + str(e))
+        client.disconnect()
 
+        return
+
+    try:
         # TODO: message request iterations
         # requesting Dcp Messages
+        msg_id = LddsMessage.IdDcpBlock
         dcp_messages = bytearray()
         while True:
-            msg_id = LddsMessage.IdDcpBlock
-            dcp_message = request_dcp_message(client, msg_id)
+            dcp_message = client.request_dcp_message(msg_id)
             c_string = get_c_string(dcp_message, 10)
             if c_string.startswith("?35"):
-                write_log(c_string)
                 break
             dcp_messages += dcp_message
         print(dcp_messages)
@@ -56,109 +64,6 @@ def test_basic_client(username: str,
         write_error(f"An error occurred: {e}")
     finally:
         client.disconnect()
-        write_log("Disconnected from server.")
-
-
-def authenticate_user(client: BasicClient,
-                      user_name: str = "user",
-                      password: str = "pass",
-                      algo: Hash = Sha1Hash(),
-                      ):
-    """
-    
-    :param client:
-    :param user_name:
-    :param password:
-    :param algo:
-    :return:
-    """
-    msg_id = LddsMessage.IdAuthHello
-    # Auth request algo (sha or sha-256) to be used based on request
-    auth_str = prepare_auth_string(user_name, password, algo)
-
-    res = request_dcp_message(client, msg_id, auth_str)
-    c_string = get_c_string(res, 10)
-    write_debug(f"C String: {c_string}")
-    # '?' means that server refused the login.
-    if len(c_string) > 0 and c_string.startswith("?"):
-        server_expn = ServerError(c_string)
-        write_debug(str(server_expn))
-        if server_expn.derr_no == 55 and isinstance(algo, Sha1Hash):
-            auth_str = prepare_auth_string(user_name, password, Sha256Hash())
-            request_dcp_message(client, msg_id, auth_str)
-        else:
-            raise Exception(f"Could not authenticate for user:{user_name}\n{server_expn}")
-
-
-def request_dcp_message(client: BasicClient,
-                        msg_id,
-                        msg_data: str = "",
-                        ) -> bytes:
-    """
-
-    :param client:
-    :param msg_id:
-    :param msg_data:
-    :return:
-    """
-    response = b""
-    message = LddsMessage(message_id=msg_id, StrData=msg_data)
-    bytes_to_send = message.get_bytes()
-    client.send_data(bytes_to_send)
-
-    try:
-        response = client.receive_data()
-    except Exception as e:
-        write_error(f"Error receiving data: {e}")
-    return response
-
-
-def prepare_auth_string(user_name: str,
-                        password: str,
-                        algo: Hash,
-                        ):
-    now = datetime.now(timezone.utc)
-    time_t = int(now.timestamp())  # Convert to Unix timestamp
-    time_str = now.strftime("%y%j%H%M%S")
-    # sha_password = PasswordFileEntry.__build_sha_password(user_name, password)
-    pfe = PasswordFileEntry(username=user_name, password=password)
-    authenticator = Authenticator(time_t, pfe, algo)
-    # Prepare the string
-    auth_string = pfe.username + " " + time_str + " " + authenticator.to_string + " " + str(14)
-    return auth_string
-
-
-def send_search_crit(client: BasicClient,
-                     filename: str,
-                     data: bytes,
-                     ):
-    """
-
-    :param client:
-    :param filename:
-    :param data:
-    :return:
-    """
-    msg = LddsMessage(message_id=LddsMessage.IdCriteria, StrData="")
-    msg.message_length = len(data) + 50
-    msg.message_data = bytearray(msg.message_length)
-
-    # Create the 'header' portion containing the search criteria filename (First 40 bytes is filename)
-    for i in range(min(40, len(filename))):
-        msg.message_data[i] = ord(filename[i])
-    msg.message_data[i] = 0
-
-    # Copy the file data into the message & send it out
-    for i in range(len(data)):
-        msg.message_data[i + 50] = data[i]
-
-    write_debug(f"Sending criteria message (filesize = {len(data)} bytes)")
-    client.send_data(msg.get_bytes())
-    try:
-        response = client.receive_data()
-        print(response)
-    except Exception as e:
-        write_error(f"Error receiving data: {e}")
 
 
 if __name__ == "__main__":
--- src/basic_client.py
+++ src/basic_client.py
@@ -1,7 +1,12 @@
 import socket
 import time
+from datetime import datetime, timezone
 from typing import Union
-from src.logs import write_debug
+from src.security import Hash, Sha1Hash, Sha256Hash, PasswordFileEntry, Authenticator
+from src.ldds_message import LddsMessage
+from src.utils.byte_util import get_c_string
+from src.exceptions.server_exceptions import ServerError
+from src.logs import write_debug, write_error
 
 
 class BasicClient:
@@ -76,6 +81,7 @@ class BasicClient:
         try:
             while True:
                 chunk = self.socket.recv(1024)  # Read in chunks of 1024 bytes
+                print(chunk)
                 if not chunk:
                     break
                 buffer.extend(chunk)
@@ -85,6 +91,92 @@ class BasicClient:
         except Exception as e:
             write_debug(f"Error receiving data: {e}")
         return bytes(buffer)
+
+    def authenticate_user(self,
+                          user_name: str = "user",
+                          password: str = "pass",
+                          ):
+        """
+
+        :param user_name:
+        :param password:
+        :return:
+        """
+        msg_id = LddsMessage.IdAuthHello
+
+        is_authenticated = False
+        for hash_algo in [Sha1Hash, Sha256Hash]:
+            auth_str = self.__prepare_auth_string(user_name, password, hash_algo())
+            res = self.request_dcp_message(msg_id, auth_str)
+            c_string = get_c_string(res, 10)
+            write_debug(f"C String: {c_string}")
+            # '?' means that server refused the login.
+            if len(c_string) > 0 and c_string.startswith("?"):
+                server_expn = ServerError(c_string)
+                write_debug(str(server_expn))
+            else:
+                is_authenticated = True
+
+        if is_authenticated:
+            write_debug(f"Authenticated user: {user_name}")
+        else:
+            raise Exception(f"Could not authenticate for user:{user_name}\n{server_expn}")
+
+    @staticmethod
+    def __prepare_auth_string(user_name: str,
+                              password: str,
+                              algo: Hash,
+                              ):
+        now = datetime.now(timezone.utc)
+        time_t = int(now.timestamp())  # Convert to Unix timestamp
+        time_str = now.strftime("%y%j%H%M%S")
+
+        pfe = PasswordFileEntry(username=user_name, password=password)
+        authenticator = Authenticator(time_t, pfe, algo)
+        # Prepare the string
+        auth_string = pfe.username + " " + time_str + " " + authenticator.to_string + " " + str(14)
+        return auth_string
+
+    def request_dcp_message(self,
+                            msg_id,
+                            msg_data: str = "",
+                            ) -> bytes:
+        """
+
+        :param msg_id:
+        :param msg_data:
+        :return:
+        """
+        response = b""
+        message = LddsMessage(message_id=msg_id, str_data=msg_data)
+        bytes_to_send = message.get_bytes()
+        self.send_data(bytes_to_send)
+
+        try:
+            response = self.receive_data()
+        except Exception as e:
+            write_error(f"Error receiving data: {e}")
+        return response
+
+    def send_search_criteria(self,
+                             data: bytes,
+                             ):
+        """
+
+        :param data:
+        :return:
+        """
+        msg = LddsMessage(message_id=LddsMessage.IdCriteria, str_data="")
+        # previously, first 50 bytes may have been used for header information including search criteria file name
+        msg.message_data = bytearray(50) + data
+
+        write_debug(f"Sending criteria message (filesize = {len(data)} bytes)")
+        self.send_data(msg.get_bytes())
+        try:
+            response = self.receive_data()
+            write_debug(response.decode())
+        except Exception as e:
+            write_error(f"Error receiving data: {e}")
 
     @property
     def name(self):

5b071a50354840840bad5b670c1a3407dd874dd1
Darshan Baral <DBaral@valleywater.org>
2024-07-03 17:31:35 -0700

    rename variable and reformat

--- src/ldds_message.py
+++ src/ldds_message.py
@@ -44,21 +44,21 @@ class LddsMessage:
     IdUnused_7 = 't'
     IdUser = 'u'
 
-    def __init__(self, hdr=None, message_id=None, StrData=None):
+    def __init__(self,
+                 hdr=None,
+                 message_id=None,
+                 str_data=None):
         if hdr is not None:
             if len(hdr) < self.ValidHdrLength:
-                raise ProtocolError(
-                    f"Invalid LDDS message header - length={len(hdr)}")
+                raise ProtocolError(f"Invalid LDDS message header - length={len(hdr)}")
             sync = bytes(ArrayUtil.get_field(hdr, 0, 4)).decode()
 
             if sync != self.ValidSync.decode():
-                raise ProtocolError(
-                    f"Invalid LDDS message header - bad sync '{sync}'")
+                raise ProtocolError(f"Invalid LDDS message header - bad sync '{sync}'")
 
             self.message_id = chr(hdr[4])
             if self.message_id not in self.ValidIds:
-                raise ProtocolError(
-                    f"Invalid LDDS message header - ID = '{self.message_id}'")
+                raise ProtocolError(f"Invalid LDDS message header - ID = '{self.message_id}'")
 
             lenbytes = ArrayUtil.get_field(hdr, 5, 5)
             for i in range(5):
@@ -70,10 +70,10 @@ class LddsMessage:
                 self.message_length = int(lenstr)
             except ValueError:
                 raise ProtocolError(f"Invalid LDDS message header - bad length field = '{lenstr}'")
-        elif message_id is not None and StrData is not None:
+        elif message_id is not None and str_data is not None:
             self.message_id = message_id
-            self.message_length = len(StrData) if StrData else 0
-            self.message_data = ArrayUtil.resize(StrData.encode(), self.message_length) \
+            self.message_length = len(str_data) if str_data else 0
+            self.message_data = ArrayUtil.resize(str_data.encode(), self.message_length) \
                 if self.message_length > 0 else None
 
     def get_bytes(self):

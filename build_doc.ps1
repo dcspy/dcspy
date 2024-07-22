885e29a4fe499d38290630e4e422ef2438152ed0
Darshan Baral <DBaral@valleywater.org>
2024-07-22 12:45:39 -0700

    update docs

--- build_doc.ps1
+++ build_doc.ps1
@@ -1,5 +1,5 @@
 $doc_dir = $args[0]
 Set-Location $doc_dir
-sphinx-apidoc -o ./ ../src/
+sphinx-apidoc -o ./ ../dcpmessagepython/
 make clean html
 make html
--- docs/modules.rst
+++ docs/modules.rst
@@ -1,7 +1,7 @@
-src
-===
+dcpmessagepython
+================
 
 .. toctree::
    :maxdepth: 4
 
-   src
+   dcpmessagepython

0f4f096bf7e84ad1b715089cb5e977e418b61b54
Darshan Baral <DBaral@valleywater.org>
2024-08-16 10:15:24 -0700

    add test for server exception

--- tests/test_server_exceptions.py
+++ tests/test_server_exceptions.py
@@ -3,8 +3,21 @@ from dcspy.server_exceptions import ServerError
 
 
 class TestServerExceptions(unittest.TestCase):
-    def test_server_error(self):
-        se = ServerError.from_error_string("?55,0,Server requires SHA-256")
+    def test_server_error_from_string(self):
+        se = ServerError.from_error_string("?55,0,Server requires SHA-256.")
         self.assertEqual(se.server_code_no, 55)
         self.assertEqual(se.system_code_no, 0)
-        self.assertEqual(se.message, "Server requires SHA-256")
+        self.assertEqual(se.message, "Server requires SHA-256.")
+
+    def test_server_error_str(self):
+        se = ServerError.from_error_string("?55,0,Server requires SHA-256.")
+        self.assertEqual(se.server_code_no, 55)
+        self.assertEqual(se.system_code_no, 0)
+        expected = ("System Code #0; Server Code #55 - Server requires SHA-256. "
+                    "(Server requires strong encryption algorithm)")
+        self.assertEqual(str(se), expected)
+
+    def test_server_error_eq(self):
+        se1 = ServerError.from_error_string("?55,0,Server requires SHA-256.")
+        se2 = ServerError("Server requires SHA-256.", 55, 0)
+        self.assertEqual(se1, se2)

32d92f0268c531a92b85b19c555f3dcedf9b4c45
Darshan Baral <DBaral@valleywater.org>
2024-12-08 20:14:31 -0800

    fix and add tests

--- tests/test_dcp_message.py
+++ tests/test_dcp_message.py
@@ -4,14 +4,47 @@ from dcspy.dcp_message import DcpMessage
 
 
 class TestDcpMessage(unittest.TestCase):
-    def test_explode(self):
-        message_data = (b"A081B07E24204153353G30-0NN096WUB00012`BST@KZ@KZh "
-                        b"A081B07E24204151853G30-0HN096WUB00012`BST@KZ@KYh "
-                        b"A081B07E24204150353G29-0HN096WUP00012`BST@KY@KYg "
-                        b"A081B07E24204144853G30-0HN096WUP00012`BST@KY@KYg ")
-        ldds_message = LddsMessage.create(LddsMessageIds.dcp_block, message_data)
-        dcp_messages = DcpMessage.explode(ldds_message)
+    def test_explode_simple_block(self):
+        message_block = (b"FAF0n00196A081B07E24204153353G30-0NN096WUB00012`BST@KZ@KZh "
+                         b"A081B07E24204151853G30-0HN096WUB00012`BST@KZ@KYh "
+                         b"A081B07E24204150353G29-0HN096WUP00012`BST@KY@KYg "
+                         b"A081B07E24204144853G30-0HN096WUP00012`BST@KY@KYg ")
+        dcp_messages = DcpMessage.explode(message_block)
         self.assertEqual(dcp_messages, ["A081B07E24204153353G30-0NN096WUB00012`BST@KZ@KZh ",
                                         "A081B07E24204151853G30-0HN096WUB00012`BST@KZ@KYh ",
                                         "A081B07E24204150353G29-0HN096WUP00012`BST@KY@KYg ",
                                         "A081B07E24204144853G30-0HN096WUP00012`BST@KY@KYg "])
+
+    def test_explode_with_end(self):
+        message_block = (b"FAF0n00196A081B07E24204153353G30-0NN096WUB00012`BST@KZ@KZh "
+                         b"A081B07E24204151853G30-0HN096WUB00012`BST@KZ@KYh "
+                         b"A081B07E24204150353G29-0HN096WUP00012`BST@KY@KYg "
+                         b"A081B07E24204144853G30-0HN096WUP00012`BST@KY@KYg "
+                         b"FAF0n00019?35,0,Until Reached"
+                         b"FAF0n00019?35,0,Until Reached"
+                         b"FAF0n00019?35,0,Until Reached"
+                         b"FAF0n00019?35,0,Until Reached"
+                         b"FAF0n00019?35,0,Until Reached"
+                         b"FAF0n00019?35,0,Until Reached"
+                         b"FAF0n00019?35,0,Until Reached")
+        dcp_messages = DcpMessage.explode(message_block)
+
+        self.assertEqual(dcp_messages, ["A081B07E24204153353G30-0NN096WUB00012`BST@KZ@KZh ",
+                                        "A081B07E24204151853G30-0HN096WUB00012`BST@KZ@KYh ",
+                                        "A081B07E24204150353G29-0HN096WUP00012`BST@KY@KYg ",
+                                        "A081B07E24204144853G30-0HN096WUP00012`BST@KY@KYg "])
+
+    def test_explode_with_two_messages_and_end(self):
+        message_block = (b"FAF0n00196A081B07E24204153353G30-0NN096WUB00012`BST@KZ@KZh "
+                         b"A081B07E24204151853G30-0HN096WUB00012`BST@KZ@KYh "
+                         b"A081B07E24204150353G29-0HN096WUP00012`BST@KY@KYg "
+                         b"A081B07E24204144853G30-0HN096WUP00012`BST@KY@KYg "
+                         b"FAF0n00049A082B07E24204153353G30-0NN096WUB00012bBST@KZ@KZh "
+                         b"FAF0n00019?35,0,Until Reached")
+        dcp_messages = DcpMessage.explode(message_block)
+
+        self.assertEqual(dcp_messages, ["A081B07E24204153353G30-0NN096WUB00012`BST@KZ@KZh ",
+                                        "A081B07E24204151853G30-0HN096WUB00012`BST@KZ@KYh ",
+                                        "A081B07E24204150353G29-0HN096WUP00012`BST@KY@KYg ",
+                                        "A081B07E24204144853G30-0HN096WUP00012`BST@KY@KYg ",
+                                        "A082B07E24204153353G30-0NN096WUB00012bBST@KZ@KZh "])

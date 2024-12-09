279b03ad0d79489f209c1148edb68f65bcc4913b
Darshan Baral <DBaral@valleywater.org>
2024-12-08 20:14:20 -0800

    use double quotes

--- tests/test_ldds_message.py
+++ tests/test_ldds_message.py
@@ -13,7 +13,7 @@ class TestLddsMessage(unittest.TestCase):
     def test_parse_create(self):
         created_message = LddsMessage.create(LddsMessageIds.dcp_block,
                                              b"A081B07E24204153353G30-0NN096WUB00012`BST@KZ@KZh ")
-        parsed_message, _ = LddsMessage.parse(b'FAF0n00049A081B07E24204153353G30-0NN096WUB00012`BST@KZ@KZh ')
+        parsed_message, _ = LddsMessage.parse(b"FAF0n00049A081B07E24204153353G30-0NN096WUB00012`BST@KZ@KZh ")
         self.assertEqual(parsed_message, created_message)
 
     def test_to_bytes(self):

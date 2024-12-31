209d3a843e7ff26589d3e905ff64955bbab6dd24
Darshan Baral <DBaral@valleywater.org>
2024-12-31 09:55:27 -0800

    add tests

---
+++ tests/test_utils.py
@@ -0,0 +1,14 @@
+import unittest
+from dcspy.utils import ByteUtil
+
+
+class TestUtils(unittest.TestCase):
+    def test_extract_string(self):
+        b = b"?55,0,Server requires SHA-256."
+        s = "Server requires SHA-256."
+        self.assertEqual(ByteUtil.extract_string(b, 6), s)
+
+    def test_extract_string_w_null(self):
+        b = b"?55,0,Server requires \x00SHA-256."
+        s = "Server requires "
+        self.assertEqual(ByteUtil.extract_string(b, 6), s)

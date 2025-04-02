6934993adc3a83ce639becbf69c02b7f7add106c
Darshan Baral <dbaral@valleywater.org>
2025-04-02 10:46:50 -0700

    delete unneeded files

--- build_package.ps1
+++
@@ -1,8 +0,0 @@
-$paths = @("dist", "build", "*.egg-info")
-foreach ($path in $paths) {
-    if (Test-Path $path) {
-        Write-Output "Removing $path"
-        Remove-Item -Recurse -Force $path
-    }
-}
-python setup.py sdist
